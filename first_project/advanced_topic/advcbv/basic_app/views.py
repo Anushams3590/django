a53bf98b03bcd3ad00f8d167ca0b59d456281e75
Anusha <anusha@macbooks-mini.home>
2024-11-21 10:57:38 +0000

    20 nov changes

--- first_project/advanced_topic/advcbv/basic_app/views.py
+++ first_project/advanced_topic/advcbv/basic_app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import View,TemplateView
+from django.views.generic import View,TemplateView,ListView,DetailView
+from . import models
 
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+"""
 class IndexView(TemplateView):
     template_name = 'index.html'
 
@@ -8,3 +13,15 @@ class IndexView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['injectme'] = 'BASIC INJECTION'
         return context
+"""
+
+class SchoolListView(ListView):
+    context_object_name = 'schools'
+    model = models.School
+
+
+class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
+    model = models.School
+    template_name = 'basic_app/school_detail.html'
+
--- first_project/clone_project/mysite/mysite/urls.py
+++ first_project/clone_project/mysite/mysite/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for advcbv project.
+URL configuration for mysite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -15,11 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from basic_app import views
+from django.urls import path
 
 urlpatterns = [
-    path('',views.IndexView.as_view()),
     path('admin/', admin.site.urls),
-    path('basic_app/',include('basic_app.urls',namespace='basic_app')),
 ]
